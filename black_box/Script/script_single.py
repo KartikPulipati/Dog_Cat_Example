3497bbda1ebdc7af154858ba97885755790ffaee
Kartik M.P <Kartik.pulipati@gmail.com>
2022-08-30 09:06:11 -0700

    Initial Commit

---
+++ black_box/Script/script_single.py
@@ -0,0 +1,28 @@
+import cv2
+import os
+import random
+import math
+
+test_or_train = ['test', 'train']
+cat_or_dog = ['CAT', 'DOG']
+directory = '../normal/CATS_DOGS'
+dump = 'CATS_DOGS_black_box_single'
+color = (0, 0, 0)
+
+
+for t in test_or_train:
+    for cd in cat_or_dog:
+        num = 1
+        for fn in os.listdir(directory+'/'+t+'/'+cd):
+            image = cv2.imread(os.path.join(directory+'/'+t+'/'+cd, fn))
+            if image is not None:
+
+                h, w = image.shape[0:2]
+
+                rh = random.randint(0, h - math.ceil(h/math.sqrt(10)))
+                rw = random.randint(0, w - math.ceil(w/math.sqrt(10)))
+
+                image = cv2.rectangle(image, (rw, rh), (rw+math.floor(w/math.sqrt(10)), rh+math.floor(h/math.sqrt(10))), color, -1)
+
+                cv2.imwrite(os.path.join(dump+'/'+t+'/'+cd, t+cd+str(num)+'.jpg'), image)
+                num += 1
---
+++ blur/Script/script_single.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+import random
+import math
+
+test_or_train = ['test', 'train']
+cat_or_dog = ['CAT', 'DOG']
+directory = '../normal/CATS_DOGS'
+dump = 'CATS_DOGS_blur_single'
+color = (0, 0, 0)
+
+
+for t in test_or_train:
+    for cd in cat_or_dog:
+        num = 1
+        for fn in os.listdir(directory+'/'+t+'/'+cd):
+            image = cv2.imread(os.path.join(directory+'/'+t+'/'+cd, fn))
+            if image is not None:
+
+                h, w = image.shape[0:2]
+
+                sizeH = math.ceil(h / math.sqrt(10))
+                sizeW = math.ceil(w / math.sqrt(10))
+
+                rh = random.randint(0, h - sizeH)
+                rw = random.randint(0, w - sizeW)
+
+                blurred_part = cv2.blur(image[rh:rh + sizeH, rw:rw + sizeW], ksize=(15, 15), )
+                image[rh:rh + sizeH, rw:rw + sizeW] = blurred_part
+
+                cv2.imwrite(os.path.join(dump+'/'+t+'/'+cd, t+cd+str(num)+'.jpg'), image)
+                num += 1
